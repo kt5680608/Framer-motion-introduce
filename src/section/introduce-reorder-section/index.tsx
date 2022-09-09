42acca7b362a8063389d1c069b1c67768e9f6238
hoon <hoon@hoonui-MacBookPro.local>
2022-09-09 15:47:37 +0900

    test: 배포 테스트

--- src/section/introduce-reorder-section/index.tsx
+++ src/section/introduce-reorder-section/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { MainContainer, IntroduceParagraph, IntroduceContainer } from '../section-styles';
 import { CustomExampleContainer } from './style';
 import { useMediaQuery } from 'react-responsive';
